6e82f6d3fde589b58d1521aa5272491edf89271b
Aaron Robinson <aaronrobinson@Aarons-MacBook-Pro.local>
2022-07-13 12:06:45 -0600

    initial push

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import './less/index.less'
 
 // Your code goes here!
+const funBus = null
+const map = null
+const canal = null
+const beach = null
+const navLinks = null
+const signUpButton = null
+const footer = null
+const text = null
+const onLoad = null
+const exit =null
